b3635550e7965e7172c2a4f5fcb941b8ec8e6683
adnanebouali <adnanbouali1@gmail.com>
2022-09-22 00:14:13 +0100

    week one

---
+++ first.py
@@ -0,0 +1,10 @@
+# Task Two
+n, a, c = "Adnane", 23, "Ageria"
+print("Name :", n, "\nAge :", a, "\nCountry :", c)
+
+# Task Three
+a, b, c = "Hello, ", "My Name Is Osama And Iam 38 Years Old ", "and I Live in Egypt."
+print(a+b+c)
+
+# Task Four
+print(type(a), "\n", type(a), "\n", type(a))
