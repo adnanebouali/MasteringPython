506e219fbc9353deca0a17b1b5075ba023d63c0a
adnanebouali <adnanbouali1@gmail.com>
2022-09-22 23:01:13 +0100

    task two

--- week two.py
+++ week two.py
@@ -2,3 +2,7 @@
 n, a, c = "Adnane", "23", "Algeria"
 print("Hello '" + n + "', How Are You Doing \\ \"\"\" Your Age Is \"" +
       a + "\"\" + and your country is:", c)
+
+# task two
+print("Hello '" + n + "', How Are You Doing \\ \n \"\"\" Your Age Is \"" +
+      a + "\"\" + \n and your country is:", c)
