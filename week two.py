3ea0c3f5701ef3904b2f5661447e522ba3327caa
adnanebouali <adnanbouali1@gmail.com>
2022-09-22 23:28:08 +0100

    task seven

--- week two.py
+++ week two.py
@@ -27,9 +27,17 @@ print(name[4::-2])
 # task five
 name = "#@#@Elzero#@#@"
 print(name.strip("#@"))
+
+# task six
 a, b, c, d, e = "1", "11", "111", "1111", "11111"
 print(a.zfill(5))
 print(b.zfill(5))
 print(c.zfill(5))
 print(d.zfill(5))
 print(e.zfill(5))
+
+# task seven
+name_one = "Osama"
+name_two = "Osama_Elzero"
+print(name_one.rjust(20, "@"))
+print(name_two.rjust(20, "@"))
