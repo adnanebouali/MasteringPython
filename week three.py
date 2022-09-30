d5e857f604629bb091de4c8d0d8dc1a53aa8469a
adnanebouali <adnanbouali1@gmail.com>
2022-10-01 00:38:41 +0100

    lists Tasks

--- week three.py
+++ week three.py
@@ -23,3 +23,60 @@ print(50 * 30)
 print(21 % 4)
 print(110 // 11)
 print(97 // 20)
+
+
+# Two
+# task one
+friends = ["Osama", "Ahmed", "Sayed", "Ali", "Mahmoud"]
+print(friends[0])
+print(friends[-1])
+
+# task two
+friends = ["Osama", "Ahmed", "Sayed", "Ali", "Mahmoud"]
+print(friends[::2])
+print(friends[1::2])
+
+# task three
+friends = ["Osama", "Ahmed", "Sayed", "Ali", "Mahmoud"]
+print(friends[1:4])
+print(friends[-2:])
+
+# task four
+friends = ["Osama", "Ahmed", "Sayed", "Ali", "Mahmoud"]
+friends[3:] = "1", "1"
+print(friends)
+
+# task five
+friends = ["Osama", "Ahmed", "Sayed", "Ali", "Mahmoud"]
+friends.insert(0, "adnane")
+friends.insert(6, "adnane")
+print(friends)
+
+# task six
+friends = ["Osama", "Ahmed", "Sayed", "Ali", "Mahmoud"]
+del friends[0:2]
+print(friends)
+del friends[-1]
+print(friends)
+
+# task seven
+friends = ["Ahmed", "Sayed"]
+employees = ["Samah", "Eman"]
+school = ["Ramy", "Shady"]
+friends.extend(employees)
+friends.extend(school)
+print(friends)
+
+# task eight
+friends.sort()
+print(friends)
+friends.sort(reverse=True)
+print(friends)
+
+# task nine
+print(len(friends))
+
+# task ten
+technologies = ["Html", "CSS", "JS", "Python", ["Django", "Flask", "Web"]]
+print(technologies[4][0])
+print(technologies[4][-1])
