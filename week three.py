58b86a3f24a23b1f9ebf4e3c0bca26c220d5c653
adnanebouali <adnanbouali1@gmail.com>
2022-09-30 22:57:44 +0100

    task one

---
+++ week three.py
@@ -0,0 +1,25 @@
+# task one
+print(3)
+print(3.5)
+print(3+5j)
+
+# task two
+val = 3+2j
+print("real num is:{}".format(val.real))
+print("imaginary num is:{}".format(val.imag))
+
+# task three
+num = 10
+print(format(num, ".10f"))
+
+# task four
+num2 = 159.650
+print(int(num2))
+print(type(int(num2)))
+
+# task five
+print(100 - 115)
+print(50 * 30)
+print(21 % 4)
+print(110 // 11)
+print(97 // 20)
