07307fe7ab0b77145843608f9b0933f1b897df6b
adnanebouali <adnanbouali1@gmail.com>
2022-10-07 23:10:35 +0100

    week five T1

---
+++ week five.py
@@ -0,0 +1,32 @@
+# task one
+print(bool(True))
+print(bool("adnane"))
+print(bool(200))
+print(bool(200.00))
+print(bool(""))
+print(bool(False))
+print(bool({}))
+print(bool([]))
+print("-" * 50)
+# task two
+html = 80
+css = 60
+javascript = 70
+print(html > 50 and css > 50 and javascript > 50)
+print("-" * 50)
+# task three
+num_one = 10
+num_two = 20
+num = 20
+print(num > num_one or num > num_two)
+print(num > num_one and num > num_two)
+print("-" * 50)
+# task four
+num_one = 10
+num_two = 20
+result = num_one+num_two
+print(result)
+print(result**3)
+print((result**3) % 2600)
+print(((result**3) % 2600)/5)
+print(type(str(((result**3) % 2600)/5)))
