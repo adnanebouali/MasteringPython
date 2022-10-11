da4639e82ad7d725db01a5fcbb1cb007a86b09b0
adnanebouali <adnanbouali1@gmail.com>
2022-10-11 22:54:28 +0100

    week seven task two

--- week seven.py
+++ week seven.py
@@ -12,3 +12,16 @@ if num > 0:
 else:
     print("The number you entered is smaller than 0")
 print("-"*30)
+# task two
+friends = ["Mohamed", "Shady", "ahmed", "eman", "Sherif"]
+i = 0
+som = 0
+while i < len(friends):
+    if friends[i] == friends[i].lower():
+        i += 1
+        som += 1
+        continue
+    print(friends[i])
+    i += 1
+print(f"Friends Printed And Ignored Names Count Is {som}")
+print("-"*30)
