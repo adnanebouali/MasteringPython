26f48577f83cfeaf973496803e4f911c0e830f3c
adnanebouali <adnanbouali1@gmail.com>
2022-10-16 21:51:44 +0100

    week eight tasks

--- week eight.py
+++ week eight.py
@@ -1,62 +1,71 @@
-# task one
-def calculate(num1, num2, op="add"):
-    op = op.lower()
-    if op == "add" or op == "a":
-        return num1+num2
-    elif op == "subtract" or op == "s":
-        return num1-num2
-    elif op == "multiply" or op == "m":
-        return num1*num2
-    else:
-        return "Op unknown"
+# # task one
+# def calculate(num1, num2, op="add"):
+#     op = op.lower()
+#     if op == "add" or op == "a":
+#         return num1+num2
+#     elif op == "subtract" or op == "s":
+#         return num1-num2
+#     elif op == "multiply" or op == "m":
+#         return num1*num2
+#     else:
+#         return "Op unknown"
 
 
-print(calculate(10, 20))  # 30
-print(calculate(10, 20, "AdD"))  # 30
-print(calculate(10, 20, "a"))  # 30
-print(calculate(10, 20, "A"))  # 30
+# print(calculate(10, 20))  # 30
+# print(calculate(10, 20, "AdD"))  # 30
+# print(calculate(10, 20, "a"))  # 30
+# print(calculate(10, 20, "A"))  # 30
 
-print(calculate(10, 20, "S"))  # -10
-print(calculate(10, 20, "subTRACT"))  # -10
+# print(calculate(10, 20, "S"))  # -10
+# print(calculate(10, 20, "subTRACT"))  # -10
 
-print(calculate(10, 20, "Multiply"))  # 200
-print(calculate(10, 20, "m"))  # 200
+# print(calculate(10, 20, "Multiply"))  # 200
+# print(calculate(10, 20, "m"))  # 200
 
-# task two
-
-
-def addition(*nums):
-    som = 0
-    for n in nums:
-        if n == 10:
-            continue
-        som += n
-        if n == 5:
-            som = som - 5
-    return som
+# # task two
 
 
-print(addition(10, 20, 30, 10, 15))  # 65
-print(addition(10, 20, 30, 10, 15, 5, 100))  # 165
+# def addition(*nums):
+#     som = 0
+#     for n in nums:
+#         if n == 10:
+#             continue
+#         som += n
+#         if n == 5:
+#             som = som - 5
+#     return som
 
 
-# task three
-def show_skills(name, *skills):
-    if skills == ():
-        print(f"Hello {name} You Have No Skills To Show")
-    else:
-        print(f"Hello {name} Your Skills Are : ")
-        for skill in skills:
-            print(f"- {skill}")
+# print(addition(10, 20, 30, 10, 15))  # 65
+# print(addition(10, 20, 30, 10, 15, 5, 100))  # 165
 
 
-show_skills("Osama", "HTML", "CSS", "JS", "Python")
-show_skills("Ahmed")
+# # task three
+# def show_skills(name, *skills):
+#     if skills == ():
+#         print(f"Hello {name} You Have No Skills To Show")
+#     else:
+#         print(f"Hello {name} Your Skills Are : ")
+#         for skill in skills:
+#             print(f"- {skill}")
 
 
-def say_hello(name="unknown", age="unknown", country="unknown"):
-    return f"Hello {name} Your Age Is {age} And You Live In {country}"
+# show_skills("Osama", "HTML", "CSS", "JS", "Python")
+# show_skills("Ahmed")
 
 
-print(say_hello("Osama", 38, "Egypt"))
-print(say_hello())
+# def say_hello(name="unknown", age="unknown", country="unknown"):
+#     return f"Hello {name} Your Age Is {age} And You Live In {country}"
+
+
+# print(say_hello("Osama", 38, "Egypt"))
+# print(say_hello())
+
+# Task one
+def get_score(**scores):
+    for subject, score in scores.items():
+        print(f"{subject} => {score}")
+
+
+get_score(Math=90, Science=80, Language=70)
+get_score(Logic=70, Problems=60)
