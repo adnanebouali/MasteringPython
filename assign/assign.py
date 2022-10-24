ab8d7812e17b34bb603919403eea19be48234e19
adnanebouali <adnanbouali1@gmail.com>
2022-10-24 22:45:34 +0100

    week 8

--- assign/assign.py
+++ assign/assign.py
@@ -1,6 +1,6 @@
 import os
 
-for num in range(1, 11):
+for num in range(1, 51):
     if num == 5:
         file = open(r"D:\my files\Python\MasteringPython-\assign\special-text.txt", "w")
         num += 1
@@ -13,3 +13,26 @@ print(os.path.dirname(os.path.abspath(__file__)))
 fileone = open(r"D:\my files\Python\MasteringPython-\assign\txt1.txt", "a")
 fileone.write("\n")
 fileone.write("Appended => Elzero Web School\n" * 50)
+
+fileone = open(r"D:\my files\Python\MasteringPython-\assign\txt1.txt")
+print(len(fileone.readlines()))
+fileone = open(r"D:\my files\Python\MasteringPython-\assign\txt1.txt")
+char = 0
+wor = 0
+lett = 0
+for word in fileone.read():
+    char += 1
+    if word == ">":
+        char -= 1
+    if word == " ":
+        wor += 1
+        char -= 1
+    if word == "l":
+        lett += 1
+print(wor + 51)
+print(char)
+print(lett)
+file.close()
+for r in range(-50, -40):
+    r = r * -1
+    os.remove(rf"D:\my files\Python\MasteringPython-\assign\txt{r}.txt")
