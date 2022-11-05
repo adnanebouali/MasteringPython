1d41f26052ba444398932c14d0b98d40fa175e6b
adnanebouali <adnanbouali1@gmail.com>
2022-11-05 14:04:56 +0100

    week ten

--- week ten.py
+++ week ten.py
@@ -14,6 +14,8 @@ for name in cleaned_list:
 cleaned_list_lambda = map(lambda name: name[1:-1:], friends_map)
 for name in cleaned_list_lambda:
     print(name)
+
+# Task two
 friends_filter = ["Osama", "Wessam", "Amal", "Essam", "Gamal", "Othman"]
 
 
@@ -21,6 +23,7 @@ def get_names(name):
     return name[-1] == "m"
 
 
+print()
 names = filter(get_names, friends_filter)
 
 for nameM in names:
@@ -29,3 +32,18 @@ for nameM in names:
 # lambda
 for nameM in filter(lambda name: name[-1] == "m", friends_filter):
     print(nameM)
+
+# task three
+from functools import reduce
+
+nums = [2, 4, 6, 2]
+
+
+def mul(num1, num2):
+    return num1 * num2
+
+
+reslt = reduce(mul, nums)
+resltLambda = reduce(lambda num1, num2: num1 * num2, nums)
+print(reslt)
+print(resltLambda)
