db9da042c4ee5522be602c83ef11ec6c2f8f7a5c
adnanebouali <adnanbouali1@gmail.com>
2022-11-05 13:52:10 +0100

    week ten

--- week ten.py
+++ week ten.py
@@ -14,3 +14,18 @@ for name in cleaned_list:
 cleaned_list_lambda = map(lambda name: name[1:-1:], friends_map)
 for name in cleaned_list_lambda:
     print(name)
+friends_filter = ["Osama", "Wessam", "Amal", "Essam", "Gamal", "Othman"]
+
+
+def get_names(name):
+    return name[-1] == "m"
+
+
+names = filter(get_names, friends_filter)
+
+for nameM in names:
+    print(nameM)
+
+# lambda
+for nameM in filter(lambda name: name[-1] == "m", friends_filter):
+    print(nameM)
