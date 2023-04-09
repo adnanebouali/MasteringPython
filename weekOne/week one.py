cae88489f183bf1b26e369482610f4653a08c0d4
adnanebouali <adnanbouali1@gmail.com>
2023-04-09 18:20:36 +0100

    Week one

---
+++ weekOne/week one.py
@@ -0,0 +1,17 @@
+# Task 1
+# this is just a comment
+# this is another comment
+
+# Task 2 , 3
+name,age,country="Adnane","23","Algeria"
+print("Name: ",name,"\nAge: ",age,"\nCountry: ",country)
+
+# Task4
+print("Hello, my name is "+name+" and i'm "+age+" I live in "+country)
+a, b, c = "Hello, ", "My Name Is Osama And Iam 38 Years Old ", "and I Live in Egypt."
+print(a+b+c)
+
+# Task5
+print(type(name))
+print(type(age))
+print(type(country))
